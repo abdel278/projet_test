df90d1316e94f168993e9a593657c360fa1b134d
Your Name <abdeloudoudmohamedsalem19@gmail.com>
2022-12-29 14:42:14 +0100

    first project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./routes/auth');
+const adminRouter = require('./routes/admin');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+
+const   DBC = "mongodb+srv://mon_sa:IoFgQtopJJrZ0jLv@cluster0.sj3wub8.mongodb.net/?retryWrites=true&w=majority";
+const PORT = 3020
+
+const app = express();
+
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+mongoose.connect(DBC).then(() => {
+    console.log('Conneced :)');
+}).catch((e) => { console.log(e); });
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`it's working in port ${PORT} `);
+});
